fde670bdef9485a8a91883ba2b3132e1d1cade10
= <=>
2018-08-01 14:30:57 -0300

    Setting up HMR

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,20 +1,15 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import Landing from './Landing'
-import Search from './Search'
-import FourOhFour from './FourOhFour'
+import App from './App'
 
-const App = () => (
-  <BrowserRouter>
-    <div className="app">
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route path="/search" component={Search} />
-        <Route component={FourOhFour} />
-      </Switch>
-    </div>
-  </BrowserRouter>
-)
+const renderApp = () => {
+  render(<App />, document.getElementById('app'))
+}
 
-render(<App />, document.getElementById('app'))
+renderApp()
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    renderApp()
+  })
+}
