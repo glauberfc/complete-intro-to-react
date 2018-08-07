7578e13f4e1a53e1d0359a9ad78e646083416b97
= <=>
2018-08-07 13:18:35 -0300

    Add body of Details component

--- js/Details.jsx
+++ js/Details.jsx
@@ -2,10 +2,32 @@
 
 import React from 'react'
 
-const Details = () => (
-  <div className="details">
-    <h1>hi lol</h1>
-  </div>
-)
+const Details = (props: { show: Show }) => {
+  const { title, description, year, poster, trailer } = props.show
+  return (
+    <div className="details">
+      <header>
+        <h1>svideo</h1>
+      </header>
+      <section>
+        <h1>{title}</h1>
+        <h2>({year})</h2>
+        <img
+          src={`/public/img/posters/${poster}`}
+          alt={`Poster for ${title}`}
+        />
+        <p>{description}</p>
+      </section>
+      <div>
+        <iframe
+          src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&amp;showinfo=0`}
+          frameBorder="0"
+          allowFullScreen
+          title={`Trailer for ${title}`}
+        />
+      </div>
+    </div>
+  )
+}
 
 export default Details
