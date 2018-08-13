ea08fd29527548f92c3df62d76c7caf808e7b77f
= <=>
2018-08-13 16:13:01 -0300

    Add Redux store to App

--- js/App.jsx
+++ js/App.jsx
@@ -1,35 +1,41 @@
 // @flow
-
+// Libraries
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import type { Match } from 'react-router-dom'
+import { Provider } from 'react-redux'
+// Components
 import Landing from './Landing'
 import Search from './Search'
 import FourOhFour from './FourOhFour'
 import Details from './Details'
+// Utils/Others
 import { shows } from '../data.json'
+import store from './store'
 
 const App = () => (
   <BrowserRouter>
-    <div className="app">
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route
-          path="/search"
-          component={props => <Search shows={shows} {...props} />}
-        />
-        <Route
-          path="/details/:id"
-          component={(props: { match: Match }) => {
-            const selectedShow = shows.find(
-              show => props.match.params.id === show.imdbID
-            )
-            return <Details show={selectedShow} {...props} />
-          }}
-        />
-        <Route component={FourOhFour} />
-      </Switch>
-    </div>
+    <Provider store={store}>
+      <div className="app">
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route
+            path="/search"
+            component={props => <Search shows={shows} {...props} />}
+          />
+          <Route
+            path="/details/:id"
+            component={(props: { match: Match }) => {
+              const selectedShow = shows.find(
+                show => props.match.params.id === show.imdbID
+              )
+              return <Details show={selectedShow} {...props} />
+            }}
+          />
+          <Route component={FourOhFour} />
+        </Switch>
+      </div>
+    </Provider>
   </BrowserRouter>
 )
 
