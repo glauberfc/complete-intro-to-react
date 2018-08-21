8f9d151f04f35ba1c394a95cbdf24d588f7461d1
= <=>
2018-08-21 09:20:58 -0300

    Setup Redux DevTools

--- js/store.js
+++ js/store.js
@@ -3,4 +3,8 @@ import thunk from 'redux-thunk'
 import reducer from './reducers'
 
 // Making a store
-export default createStore(reducer, applyMiddleware(thunk))
+export default createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), // eslint-disable-line
+  applyMiddleware(thunk)
+)
